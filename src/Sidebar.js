02824a424728cc6c5b41d666a5c17f331d5610bc
Nitin N <nitinnarayanan77@gmail.com>
2020-10-26 21:17:38 +0530

    sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import './Sidebar.css'
+import SidebarOption from './SidebarOption'
+import HomeIcon from "@material-ui/icons/Home"
+import SearchIcon from "@material-ui/icons/Search"
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic"
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>I am the sidebar</h1>
+            <img className="sidebar_logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" />
+
+            <SidebarOption title="Home" Icon = {HomeIcon}/>
+            <SidebarOption title="Search" Icon = {SearchIcon}/>
+            <SidebarOption title="Library" Icon = {LibraryMusicIcon}/>
+            <br />
+            <strong className="sidebar_title">PLAYLISTS</strong>
+            <hr />
         </div>
     )
 }
