771d0ad3031e40209f6748217f9ce822e9bce6f9
Nitin N <nitinnarayanan77@gmail.com>
2020-10-26 19:46:37 +0530

    datalayer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from './Player'
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -13,10 +17,21 @@ function App() {
 
     if (_token) {
       setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        console.log('user:',user);
+      })
     }
   }, []);
 
-  return <div className="App">{token ? <h1>Logged in.</h1> : <Login />}</div>;
+  return <div className="App">
+    {token ?
+    <Player /> 
+    : 
+    <Login />}
+    </div>;
 }
 
 export default App;
