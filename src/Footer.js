734a375588cadb6d0bfccc0d709cc81abf026ec3
Nitin N <nitinnarayanan77@gmail.com>
2020-10-27 14:43:56 +0530

    footer p1

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,45 @@
-import React from 'react'
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import RepeatShuffleIcon from "@material-ui/icons/Repeat";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import {Grid, Slider} from "@material-ui/core";
+
 
 function Footer() {
-    return (
-        <div className="footer">
-            <h1>Footer</h1>
-        </div>
-    )
+  return (
+    <div className="footer">
+      <div className="footer_left">
+        <p>Album and song</p>
+      </div>
+      <div className="footer_center">
+        <ShuffleIcon className="footer_green" />
+        <SkipPreviousIcon className="footer_icon" />
+        <PlayCircleOutlineIcon fontSize="large" className="footer_icon" />
+        <SkipNextIcon className="footer_icon" />
+        <RepeatShuffleIcon className="footer_green" />
+      </div>
+      <div className="footer_right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider aria-labelledby="continuous-slider" />
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
